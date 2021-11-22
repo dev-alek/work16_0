c78eebfe9f255f402be015e48b3bc2de781f956a
DRuban <none@none>
2021-11-22 19:49:14 +0300

    Создание bat файлов th
    --HG--
    branch : rls

--- runth/CrLink.bat
+++ runth/CrLink.bat
@@ -1,4 +1,4 @@
-cd c:\wrk\16_0
-start c:\dlc1172\bin\prowin32.exe   -p thrun.p -debugalert
+cd ..
+start c:\dlc1172\bin\prowin32.exe   -p runth\thrun.p -debugalert
 rem -clientlog  mylog.lg -logginglevel 4 -logentrytypes 4GLTrace 
 EXIT
